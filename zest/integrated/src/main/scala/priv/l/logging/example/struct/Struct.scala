25bc2eb807467aa370d60b41390c5c1fcc933fd8
kunfu <kunfu@paypal.com>
2018-10-08 15:13:47 +0800

    refact codes

--- zest/integrated/src/main/scala/priv/l/logging/example/struct/Struct.scala
+++ zest/integrated/src/main/scala/priv/l/logging/example/struct/Struct.scala
@@ -2,6 +2,6 @@ package priv.l.logging.example.struct
 
 sealed class Struct
 
-case class MyStruct1(a: String, b: Int, c: Double, d: List[String]) extends Struct
+final case class MyStruct1(a: String, b: Int, c: Double, d: List[String]) extends Struct
 
-case class MyStruct2(cnt: Int, tp: String, freq: Double, nums: List[Int]) extends Struct
+final case class MyStruct2(cnt: Int, tp: String, freq: Double, nums: List[Int]) extends Struct
